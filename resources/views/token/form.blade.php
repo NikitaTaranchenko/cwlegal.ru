a0fc7fa5ac03e2e44ba2798ab90ddca151c53bce
Nikita Taranchenko <taranchenko@me.com>
2015-02-23 22:06:31 +0000

    Fixes

--- resources/views/token/form.blade.php
+++ resources/views/token/form.blade.php
@@ -11,18 +11,22 @@
 
     <!-- Form to request token for login into the secure area -->
     <div class="panel panel-default">
+
         <div class="panel panel-heading">
             <h1>{{ trans('login.requestToken') }}</h1>
             <small>{{ trans('login.requestTokenHelper').'.' }}</small>
         </div>
+
         <div class="panel panel-body">
             {!! Form::open() !!}
+
             <div class="form-group">
                 {!! Form::label('email', trans('login.email').':') !!}
                 {!! Form::email('email', null, array(
                     'class'=>'form-control'
                 )) !!}
             </div>
+
             <div class="form-group">
                 {!! Form::submit(trans('login.submit'), array(
                     'class'=>' btn btn-primary'
@@ -30,6 +34,7 @@
             </div>
             {!! Form::close() !!}
         </div>
+
     </div>
 
 @endsection
