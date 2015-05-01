e1b8c7fce13a1c142a5e4c118a4020135eca85ec
Nikita Taranchenko <taranchenko@me.com>
2015-05-01 19:10:41 +0000

    Update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-        $this->call('TokenTableSeeder');
-        $this->command->info('Tokens table seeded!');
+//        $this->call('TokenTableSeeder');
+//        $this->command->info('Tokens table seeded!');
 		// $this->call('UserTableSeeder');
 	}
 
